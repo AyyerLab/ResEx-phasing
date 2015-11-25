f6ded627bf042d046fd9851796317f165624f9a1
Kartik Ayyer <kartik.ayyer@desy.de>
2015-11-25 10:44:46 +0100

    calc_scale now compares symmetrized intensity with data

--- utils/src/calc_scale.c
+++ utils/src/calc_scale.c
@@ -1,38 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <complex.h>
 
 int main(int argc, char *argv[]) {
 	long x, y, z, size, c, vol, num_vox = 0 ;
 	double dot = 0., normsq = 0., rsq, scale, rmin, rmax ;
 	float *obs_mag, *model_mag ;
-	float complex *model ;
 	FILE *fp ;
 	
 	if (argc < 5) {
-		fprintf(stderr, "Format: %s <model_fname> <merge_fname> <rmin> <rmax>\n", argv[0]) ;
+		fprintf(stderr, "Format: %s <sym_model_fname> <merge_fname> <rmin> <rmax>\n", argv[0]) ;
 		return 1 ;
 	}
 	rmin = atof(argv[3]) ;
 	rmax = atof(argv[4]) ;
 	
-	size = 501 ;
+	size = 701 ;
 	c = size / 2 ;
 	vol = size*size*size ;
 	
 	// Parse complex model
-	model = malloc(vol * sizeof(float complex)) ;
+	model_mag = malloc(vol * sizeof(float)) ;
 	fp = fopen(argv[1], "rb") ;
-	fread(model, sizeof(float complex), vol, fp) ;
+	fread(model_mag, sizeof(float), vol, fp) ;
 	fclose(fp) ;
 	
 	// Calculate magnitude
-	model_mag = malloc(vol * sizeof(float)) ;
 	for (x = 0 ; x < vol ; ++x)
-		model_mag[x] = cabsf(model[x]) ;
+		model_mag[x] = sqrt(model_mag[x]) ;
 	
-	free(model) ;
 	
 	// Parse experimental merge
 	obs_mag = malloc(vol * sizeof(float)) ;
