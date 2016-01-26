619a042473415edad0a4a6a65c7a49eccbfeae54
Kartik Ayyer <kartik.ayyer@desy.de>
2016-01-26 17:12:52 +0100

    process_map.sh: Combines various utilities to process ccp4 map

---
+++ process_map.sh
@@ -0,0 +1,43 @@
+#!/bin/zsh
+
+if [ $# -lt 2 ]
+then
+	echo Format: $0 map size res_at_edge
+	exit
+fi
+
+size=$2
+res_at_edge=$3
+rad=$(( size / 2 ))
+res=$(( 1. * res_at_edge * rad ))
+
+map_name=`basename $1`
+mapnoext="${map_name%.*}"
+log_name=${mapnoext}.log
+
+echo $log_name
+echo ./utils/read_map $1 $res
+./utils/read_map $1 $res &>$log_name
+
+sx=`grep Stretch $log_name|awk -F'[=,()]' '{print $3}'`
+sy=`grep Stretch $log_name|awk -F'[=,()]' '{print $4}'`
+sz=`grep Stretch $log_name|awk -F'[=,()]' '{print $5}'`
+padmodel=`grep "Saving padded" $log_name|awk '{print $5}'`
+padsize=`grep "volume size" $log_name|awk '{print $5}'`
+padnoext="${padmodel%.*}"
+strmodel=data/${mapnoext}-str.cpx
+lowresmodel_name=data/${mapnoext}-recon.raw
+supp_name=data/${mapnoext}-3.supp
+
+echo ./utils/gen_map $padmodel $padsize
+./utils/gen_map $padmodel $padsize &> $log_name
+
+echo ./utils/fstretch ${padnoext}-fdens.cpx $padsize $size $sx $sy $sz $strmodel
+./utils/fstretch ${padnoext}-fdens.cpx $padsize $size $sx $sy $sz $strmodel &> $log_name
+
+echo ./utils/gen_dens $strmodel $size $lowresmodel_name
+./utils/gen_dens $strmodel $size $lowresmodel_name
+
+echo ./utils/create_support $lowresmodel_name $size 3. 5 $supp_name
+./utils/create_support $lowresmodel_name $size 3. 5 $supp_name &> $log_name
+
