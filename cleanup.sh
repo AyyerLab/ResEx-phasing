3a61c7f6216cb2c2d37283b3293247f562cddbff
Kartik Ayyer <kartik.ayyer@desy.de>
2016-05-24 11:58:27 +0200

    cleanup.sh: Works with new gen_map format

--- cleanup.sh
+++ cleanup.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/zsh
 
 i=1
 while [ $i -lt 100 ]
 do
-	printf -v j "%.2d" $i
+	j=`printf "%.2d" $i`
 	if [ -f results/output_${j}.raw ]
 	then
 		((i++))
@@ -24,10 +24,9 @@ cp src/config.ini results/conf_${j}.ini
 #printf "output_%.2d.raw\t(600,400)\t0.6\t\tSame as \`29', but tight support\n" $i >> results/KEY
 echo Generating map
 supp_fname=`grep support_fname src/config.ini|awk '{print $3}'`
-./utils/gen_map results/output_${j}.raw 301 300. $supp_fname
+echo support = $supp_fname
+./utils/gen_map results/output_${j}.raw 301 1. 1. 1. $supp_fname
 cd data/maps
 rm -f output_${j}.mtz
 phenix.map_to_structure_factors output_${j}.map.ccp4 box=True output_file_name=output_${j}.mtz
-cd ..
-cd ..
-pwd
+cd -
