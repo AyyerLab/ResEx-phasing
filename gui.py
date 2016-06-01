48206351b18f8ad52c7305fa6edfa14e53416068
Kartik Ayyer <kartik.ayyer@desy.de>
2016-06-01 19:28:57 +0200

    gui: Many updates. Can now view output of map processing

--- gui.py
+++ gui.py
@@ -21,6 +21,7 @@ class GUI():
         rangemin = 0.
         self.merge_fname = Tk.StringVar()
         self.map_fname = Tk.StringVar()
+        self.fname = Tk.StringVar()
         self.rangeminstr = Tk.StringVar()
         self.rangemaxstr = Tk.StringVar()
         self.imagename = Tk.StringVar()
@@ -29,16 +30,18 @@ class GUI():
         self.radiusmax = Tk.StringVar()
         self.resedge = Tk.StringVar()
         self.circleflag = Tk.IntVar()
+        self.rangelock = Tk.IntVar()
         self.map_image_exists = False
         self.vol_image_exists = False
 
         self.merge_fname.set(merge_fname)
         self.map_fname.set(map_fname)
-        self.fname = merge_fname
-        self.imagename.set('images/' + os.path.splitext(os.path.basename(self.fname))[0] + '.png')
+        self.fname.set(merge_fname)
+        self.imagename.set('images/' + os.path.splitext(os.path.basename(self.fname.get()))[0] + '.png')
         self.radiusmin.set('0')
         self.radiusmax.set('0')
         self.circleflag.set(0)
+        self.rangelock.set(0)
         self.size = None
         self.old_fname = None
         self.space = None
@@ -46,6 +49,7 @@ class GUI():
         self.rangemaxstr.set("%.1e" % rangemax)
         self.layernum.set(0)
         self.zeroed = False
+        self.calculated_scale = False
         self.processed_map = False
 
         self.master.tk.eval('source [file join themes plastik plastik.tcl]')
@@ -60,30 +64,37 @@ class GUI():
         self.master.bind('<KP_Enter>', self.replot)
         self.master.bind('<Control-s>', self.save_plot)
         self.master.bind('<Control-q>', self.quit_)
+        self.master.bind('<Right>', self.increment_layer)
+        self.master.bind('<Left>', self.decrement_layer)
         self.master.bind('<Up>', self.increment_layer)
         self.master.bind('<Down>', self.decrement_layer)
+        self.master.bind('<Control-m>', lambda event: self.plot_vol(fname=self.merge_fname.get()))
+        self.master.bind('<Control-n>', lambda event: self.plot_map())
         self.master.rowconfigure(0, weight=1)
         self.master.columnconfigure(0, weight=1)
 
-        self.fig = plt.figure(figsize=(15,5))
-        self.fig.subplots_adjust(left=0.0, bottom=0.00, right=0.99, wspace=0.0)
+        # Canvas frame
         canvas_frame = ttk.Frame(self.master)
         canvas_frame.grid(row=0, column=0, sticky='news')
         canvas_frame.rowconfigure(0, weight=1)
         canvas_frame.columnconfigure(0, weight=1)
+        
+        line = ttk.Frame(canvas_frame)
+        line.pack(fill=Tk.X)
+        ttk.Label(line, textvariable=self.fname).pack(side=Tk.LEFT)
+        self.fig = plt.figure(figsize=(15,5))
+        self.fig.subplots_adjust(left=0.0, bottom=0.00, right=0.99, wspace=0.0)
         self.canvas = FigureCanvasTkAgg(self.fig, canvas_frame)
         self.canvas.show()
         self.canvas.get_tk_widget().pack(fill='both', expand=1)
 
+        # Config frame
         config_frame = ttk.Frame(self.master)
         config_frame.grid(row=0, column=1, sticky='news')
 
-        n = ttk.Notebook(config_frame)
-        n.pack(fill=Tk.X)
-        self.merge_frame = ttk.Frame(n)
-        self.map_frame = ttk.Frame(n)
-        n.add(self.merge_frame, text='Merge')
-        n.add(self.map_frame, text='Map')
+        self.notebook = ttk.Notebook(config_frame)
+        self.notebook.pack(fill=Tk.X)
+        self.notebook.enable_traversal()
         self.gen_merge_tab()
         self.gen_map_tab()
 
@@ -108,6 +119,7 @@ class GUI():
         ttk.Label(line,text="Range: ").pack(side=Tk.LEFT)
         ttk.Entry(line,textvariable=self.rangeminstr,width=10).pack(side=Tk.LEFT)
         ttk.Entry(line,textvariable=self.rangemaxstr,width=10).pack(side=Tk.LEFT)
+        ttk.Checkbutton(line,text='Lock',variable=self.rangelock).pack(side=Tk.LEFT)
 
         line = ttk.Frame(config_frame)
         line.pack(fill=Tk.X)
@@ -116,6 +128,9 @@ class GUI():
         self.plot_vol()
 
     def gen_map_tab(self):
+        self.map_frame = ttk.Frame(self.notebook)
+        self.notebook.add(self.map_frame, text='Map')
+
         line = ttk.Frame(self.map_frame)
         line.pack(fill=Tk.X)
         ttk.Label(line,text="Map File:").pack(side=Tk.LEFT)
@@ -128,7 +143,15 @@ class GUI():
         ttk.Entry(line,textvariable=self.resedge,width=5).pack(side=Tk.LEFT)
         ttk.Button(line,text="Process Map",command=self.process_map).pack(side=Tk.LEFT)
 
+    def reset_map_tab(self, event=None):
+        self.map_frame.destroy()
+        self.gen_map_tab()
+        self.notebook.insert(1, self.map_frame)
+
     def gen_merge_tab(self):
+        self.merge_frame = ttk.Frame(self.notebook)
+        self.notebook.add(self.merge_frame, text='Merge')
+
         line = ttk.Frame(self.merge_frame)
         line.pack(fill=Tk.X)
         ttk.Label(line, text="Merge File: ").pack(side=Tk.LEFT)
@@ -146,12 +169,21 @@ class GUI():
         line.pack(fill=Tk.X)
         ttk.Button(line,text='Zero Outer',command=self.zero_outer).pack(side=Tk.LEFT)
         ttk.Button(line,text='Calc. Scale',command=self.calc_scale).pack(side=Tk.LEFT)
+        ttk.Button(line,text='Reset',command=self.reset_merge_tab).pack(side=Tk.LEFT)
+
+    def reset_merge_tab(self, event=None):
+        self.zeroed = False
+        self.calculated_scale = False
+        self.merge_frame.destroy()
+        self.gen_merge_tab()
+        self.notebook.insert(0, self.merge_frame)
+        self.notebook.select(self.notebook.tabs()[0])
 
     def replot(self, event=None):
         if self.map_image_exists:
             self.plot_map()
         elif self.vol_image_exists:
-            self.plot_vol()
+            self.plot_vol(fname=self.fname.get())
         else:
             self.plot_vol() # Default plotting merge
 
@@ -165,7 +197,8 @@ class GUI():
         elif ext_string == '.supp':
             print "Support file"
             self.typestr = 'uint8'
-            self.rangemaxstr.set('%.1e' % 1)
+            if self.rangelock.get() == 0:
+                self.rangemaxstr.set('%.1e' % 1)
         elif ext_string == '.cpx':
             print "Complex file"
             self.typestr = 'complex64'
@@ -174,11 +207,11 @@ class GUI():
             self.typestr = 'f4'
 
     def parse_vol(self):
-        if not os.path.isfile(self.fname):
-            print "Unable to open", self.fname 
+        if not os.path.isfile(self.fname.get()):
+            print "Unable to open", self.fname.get()
             return
-        self.parse_extension(self.fname)
-        self.vol = np.fromfile(self.fname, dtype=self.typestr)
+        self.parse_extension(self.fname.get())
+        self.vol = np.fromfile(self.fname.get(), dtype=self.typestr)
         size = int(round(self.vol.size**(1/3.)))
         self.size = size
         self.vol_size = size
@@ -187,15 +220,16 @@ class GUI():
         if self.typestr == 'complex64':
             self.vol = np.square(np.absolute(self.vol))
         self.slider.configure(from_=0,to=self.size-1)
+        if self.rangelock.get() == 0:
+            self.rangemaxstr.set('%.1e' % (5*self.vol[self.vol>0].mean()))
         if not self.vol_image_exists:
-            self.rangemaxstr.set('%.1e' % (5*self.vol.mean()))
             self.layernum.set(self.size/2)
             self.radiusmin.set('%d' % (self.size/2/2))
             self.radiusmax.set('%d' % (self.size/2))
-        self.old_fname = self.fname
+        self.old_fname = self.fname.get()
 
     def parse_map(self):
-        f = open(self.fname, 'rb')
+        f = open(self.fname.get(), 'rb')
         f.seek(28, 0)
         grid = np.fromfile(f, '=i4', count=3)
         nx, ny, nz = tuple(grid)
@@ -210,18 +244,25 @@ class GUI():
         self.slider.configure(from_=0,to=self.size-1)
         if not self.map_image_exists:
             self.layernum.set(self.size/2)
-        self.old_fname = self.fname
+        self.old_fname = self.fname.get()
 
-    def plot_slices(self, layernum, space=None):
+    def plot_slices(self, layernum, space=None, zoom=False):
         if space is None:
             space = self.space
-        self.imagename.set('images/' + os.path.splitext(os.path.basename(self.fname))[0] + '.png')
+        self.imagename.set('images/' + os.path.splitext(os.path.basename(self.fname.get()))[0] + '.png')
         rangemax = float(self.rangemaxstr.get())
         rangemin = float(self.rangeminstr.get())
         
-        a = self.vol[layernum,:,:]	
-        b = self.vol[:,layernum,:]	
-        c = self.vol[:,:,layernum]
+        if zoom:
+            min = self.size/3
+            max = 2*min
+            a = self.vol[layernum,min:max,min:max]
+            b = self.vol[min:max,layernum,min:max]
+            c = self.vol[min:max,min:max,layernum]
+        else:
+            a = self.vol[layernum,:,:]	
+            b = self.vol[:,layernum,:]	
+            c = self.vol[:,:,layernum]
             
         s1 = self.fig.add_subplot(131)
         s1.matshow(a, vmin=rangemin, vmax=rangemax, cmap='jet')
@@ -262,26 +303,29 @@ class GUI():
         self.space = space
         self.canvas.show()
 
-    def plot_vol(self, event=None):
-        self.fname = self.merge_fname.get()
+    def plot_vol(self, fname=None, zoom=False, event=None):
+        if fname is None:
+            self.fname.set(self.merge_fname.get())
+        else:
+            self.fname.set(fname)
         if not self.vol_image_exists:
             self.parse_vol()
-            #self.rangemaxstr.set('%.1e' % 1.e8)
-        elif self.old_fname != self.fname: 
-            print "Reparsing volume:", self.fname
+        elif self.old_fname != self.fname.get(): 
+            print "Reparsing volume:", self.fname.get()
             self.parse_vol()
         
-        self.plot_slices(self.layernum.get(), space='fourier')
+        self.plot_slices(self.layernum.get(), space='fourier', zoom=zoom)
         self.vol_image_exists = True
         self.map_image_exists = False
 
     def plot_map(self, event=None):
-        self.fname = self.map_fname.get()
+        self.fname.set(self.map_fname.get())
         if not self.map_image_exists:
             self.parse_map()
-            self.rangemaxstr.set('%.1e' % 10)
-        elif self.old_fname != self.fname:
-            print "Reparsing map:", self.fname
+            if self.rangelock.get() == 0:
+                self.rangemaxstr.set('%.1e' % 10)
+        elif self.old_fname != self.fname.get():
+            print "Reparsing map:", self.fname.get()
             self.parse_map()
         self.plot_slices(self.layernum.get(), space='real')
         self.map_image_exists = True
@@ -293,15 +337,13 @@ class GUI():
         print '-'*80
         os.system('./utils/zero_outer %s %d %d %d' % (self.merge_fname.get(), self.size, rmin, rmax))
         print '-'*80
-        self.fname = os.path.splitext(self.merge_fname.get())[0] + '-zero.raw'
-        self.parse_vol()
-        self.plot_slices(self.layernum.get(), space='fourier')
         
         if not self.zeroed:
-            ttk.Separator(self.merge_frame, orient=Tk.HORIZONTAL).pack(fill=Tk.X, padx=5, pady=5)
+            #ttk.Separator(self.merge_frame, orient=Tk.HORIZONTAL).pack(fill=Tk.X, padx=5, pady=5)
             line = ttk.Frame(self.merge_frame)
             line.pack(fill=Tk.X)
-            ttk.Label(line,text='Zero-ed volume: %s' % self.fname).pack(side=Tk.LEFT)
+            ttk.Label(line,text='Zero-ed volume: %s' % self.fname.get()).pack(side=Tk.LEFT,fill=Tk.X,expand=1)
+            ttk.Button(line,text='Plot',command=lambda: self.plot_vol(fname=os.path.splitext(self.merge_fname.get())[0] + '-zero.raw')).pack(side=Tk.LEFT)
         self.zeroed = True
 
     def process_map(self, event=None):
@@ -312,6 +354,7 @@ class GUI():
         if os.path.isfile('data/'+mapnoext+'.cpx'):
             if not tkMessageBox.askyesno('Process Map', 'Found processed map output. Overwrite?', default=tkMessageBox.NO, icon=tkMessageBox.QUESTION, parent=self.merge_frame):
                 self.processed_map = True
+                self.add_to_map_frame(mapnoext)
                 return
         if self.resedge.get() is '':
             print 'Need resolution at edge of volume'
@@ -320,6 +363,7 @@ class GUI():
         os.system('./process_map.sh %s %d %f' % (self.map_fname.get(), self.vol_size, float(self.resedge.get())))
         print '-'*80
         self.processed_map = True
+        self.add_to_map_frame(mapnoext)
 
     def calc_scale(self, event=None):
         rmin = int(self.radiusmin.get())
@@ -327,11 +371,34 @@ class GUI():
         mapnoext = os.path.splitext(os.path.basename(self.map_fname.get()))[0]
         sym_model = 'data/'+mapnoext+'-sym.raw'
         cmd = './utils/calc_scale %s %s %d %d %d' % (sym_model, self.merge_fname.get(), self.size, rmin, rmax)
-        print '-'*80
         output = subprocess.check_output(cmd.split(), shell=False)
-        self.scale_factor = output.split()[4]
-        print 'scale_factor =', self.scale_factor
-        print '-'*80
+        self.scale_factor = float(output.split()[4])
+        if not self.calculated_scale:
+            line = ttk.Frame(self.merge_frame)
+            line.pack(fill=Tk.X)
+            ttk.Label(line,text='Scale factor = %.6e'%self.scale_factor).pack(side=Tk.LEFT)
+        self.calculated_scale = True
+
+    def add_to_map_frame(self, mapnoext):
+        line = ttk.Frame(self.map_frame)
+        line.pack(fill=Tk.X)
+        ttk.Label(line,text='Complex: '+'data/'+mapnoext+'.cpx ').pack(side=Tk.LEFT,fill=Tk.X,expand=1)
+        ttk.Button(line,text='Plot',command=lambda: self.plot_vol(fname='data/'+mapnoext+'.cpx')).pack(side=Tk.LEFT)
+        
+        line = ttk.Frame(self.map_frame)
+        line.pack(fill=Tk.X)
+        ttk.Label(line,text='Symmetrized: '+'data/'+mapnoext+'-sym.raw ').pack(side=Tk.LEFT,fill=Tk.X,expand=1)
+        ttk.Button(line,text='Plot',command=lambda: self.plot_vol(fname='data/'+mapnoext+'-sym.raw')).pack(side=Tk.LEFT)
+        
+        line = ttk.Frame(self.map_frame)
+        line.pack(fill=Tk.X)
+        ttk.Label(line,text='Density: '+'data/'+mapnoext+'-srecon.raw ').pack(side=Tk.LEFT,fill=Tk.X,expand=1)
+        ttk.Button(line,text='Plot',command=lambda: self.plot_vol(fname='data/'+mapnoext+'-srecon.raw', zoom=True)).pack(side=Tk.LEFT)
+        
+        line = ttk.Frame(self.map_frame)
+        line.pack(fill=Tk.X)
+        ttk.Label(line,text='Support: '+'data/'+mapnoext+'-3.supp ').pack(side=Tk.LEFT,fill=Tk.X,expand=1)
+        ttk.Button(line,text='Plot',command=lambda: self.plot_vol(fname='data/'+mapnoext+'-3.supp', zoom=True)).pack(side=Tk.LEFT)
 
     def increment_layer(self, event=None):
         self.layernum.set(min(self.layernum.get()+1, self.size))
