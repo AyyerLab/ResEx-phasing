804f60c088bf26d030a44020942a42320b44a5ce
Kartik Ayyer <kartik.ayyer@desy.de>
2016-06-01 16:57:34 +0200

    process_map: Fixed bug in stretch factor parsing

--- process_map.sh
+++ process_map.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/usr/bin/env zsh
 
 if [ $# -lt 3 ]
 then
@@ -20,13 +20,8 @@ res=$(( 1. * res_at_edge * rad ))
 map_name=`basename $1`
 mapnoext="${map_name%.*}"
 log_name=results/${mapnoext}.log
+echo Log file: $log_name
 
-sx=`grep Stretch $log_name|awk -F'[=,()]' '{print $3}'`
-sy=`grep Stretch $log_name|awk -F'[=,()]' '{print $4}'`
-sz=`grep Stretch $log_name|awk -F'[=,()]' '{print $5}'`
-padmodel=`grep "Saving padded" $log_name|awk '{print $5}'`
-padsize=`grep "volume size" $log_name|awk '{print $5}'`
-padnoext="${padmodel%.*}"
 strmodel=data/${mapnoext}-str.cpx
 lowresmodel_name=data/${mapnoext}-recon.raw
 supp_name=data/${mapnoext}-3.supp
@@ -34,22 +29,33 @@ supprecon_name=data/${mapnoext}-srecon.raw
 supp_radius=3
 supp_thresh=10
 
-echo skip = $skip
-echo Log file: $log_name
+if [ $skip -eq 0 ]
+then
+	echo Doing entire pipeline
+elif [ $skip -lt 2 ]
+then
+	echo Only doing support wrapping
+fi
 
 if [ $skip -eq 0 ]
 then
 	echo ./utils/read_map $1 $res | tee $log_name
 	./utils/read_map $1 $res &>> $log_name
-
+	
+	padmodel=`grep "Saving padded" $log_name|awk '{print $5}'`
+	padsize=`grep "volume size" $log_name|awk '{print $5}'`
+	padnoext="${padmodel%.*}"
 	echo -------------------------------------------------------------------------------- >> $log_name
 	echo ./utils/gen_fdens $padmodel $padsize | tee -a $log_name
 	./utils/gen_fdens $padmodel $padsize &>> $log_name
-
+	
+	sx=`grep Stretch $log_name|awk -F'[=,()]' '{print $3}'`
+	sy=`grep Stretch $log_name|awk -F'[=,()]' '{print $4}'`
+	sz=`grep Stretch $log_name|awk -F'[=,()]' '{print $5}'`
 	echo -------------------------------------------------------------------------------- >> $log_name
 	echo ./utils/fstretch ${padnoext}-fdens.cpx $padsize $size $sx $sy $sz $strmodel | tee -a $log_name
 	./utils/fstretch ${padnoext}-fdens.cpx $padsize $size $sx $sy $sz $strmodel &>> $log_name
-
+	
 	echo -------------------------------------------------------------------------------- >> $log_name
 	echo ./utils/gen_dens $strmodel $size $lowresmodel_name | tee -a $log_name
 	./utils/gen_dens $strmodel $size $lowresmodel_name &>> $log_name
@@ -60,7 +66,7 @@ then
 	echo -------------------------------------------------------------------------------- >> $log_name
 	echo ./utils/create_support $lowresmodel_name $size $supp_radius $supp_thresh $supp_name | tee -a $log_name
 	./utils/create_support $lowresmodel_name $size $supp_radius $supp_thresh $supp_name &>> $log_name
-
+	
 	echo -------------------------------------------------------------------------------- >> $log_name
 	echo Constraining lowresmodel by support | tee -a $log_name
 	python <<-EOF
@@ -70,13 +76,14 @@ then
 	m *= s
 	m.tofile('$supprecon_name')
 	EOF
-
+	
 	echo -------------------------------------------------------------------------------- >> $log_name
 	echo ./utils/gen_fdens $supprecon_name $size data/${mapnoext}.cpx | tee -a $log_name
 	./utils/gen_fdens $supprecon_name $size data/${mapnoext}.cpx &>> $log_name
-
+	
 	echo -------------------------------------------------------------------------------- >> $log_name
 	supp_name=data/${mapnoext}-$supp_radius.supp
 	echo ./utils/create_support $lowresmodel_name $size $supp_radius $supp_thresh $supp_name | tee -a $log_name
 	./utils/create_support $lowresmodel_name $size $supp_radius $supp_thresh $supp_name &>> $log_name
 fi
+
