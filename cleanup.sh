ccbd8ef62633371bcaf5f1ef0cda56bed32f6907
Kartik Ayyer <kartik.ayyer@desy.de>
2016-06-10 18:55:11 +0200

    cleanup.sh: Now locates output files from output_prefix flag in config.ini

--- cleanup.sh
+++ cleanup.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 i=1
-while [ $i -lt 100 ]
+while [ $i -lt 200 ]
 do
 	j=`printf "%.2d" $i`
 	if [ -f results/output_${j}.raw ]
@@ -12,20 +12,29 @@ do
 	fi
 done
 
-echo Cleaning to number $j...
-#echo Give results message
-#read comment
-cp data/recon.raw results/output_${j}.raw
-cp data/frecon.raw results/foutput_${j}.raw
-cp PHASING.log results/log_${j}.dat
-cp prtf.dat results/prtf_${j}.dat
-cp src/config.ini results/conf_${j}.ini
-#printf "output_%.2d.raw\t(600,400)\t0.6\t\tSame as \`29', but tight support\n" $i
-#printf "output_%.2d.raw\t(600,400)\t0.6\t\tSame as \`29', but tight support\n" $i >> results/KEY
+set -e
+if [ $# -gt 0 ]
+then
+	if [ $1 = '-v' ]
+	then
+		set -x
+	fi
+fi
+
+
+prefix=`grep output_prefix config.ini|awk -F"[ =]" '{print $2}'`
+voxsize=$((400. / 350.))
+
+echo Cleaning to number $j with data from $prefix
+cp ${prefix}-recon.raw results/output_${j}.raw
+cp ${prefix}-frecon.raw results/foutput_${j}.raw
+cp ${prefix}-log.dat results/log_${j}.dat
+cp ${prefix}-prtf.dat results/prtf_${j}.dat
+cp config.ini results/conf_${j}.ini
 echo Generating map
-supp_fname=`grep support_fname src/config.ini|awk '{print $3}'`
+supp_fname=`grep support_fname config.ini|awk -F"[ =]" '{print $2}'`
 echo support = $supp_fname
-./utils/gen_map results/output_${j}.raw 301 1. 1. 1. $supp_fname
+./utils/gen_map results/output_${j}.raw 701 $voxsize $voxsize $voxsize $supp_fname
 cd data/maps
 rm -f output_${j}.mtz
 phenix.map_to_structure_factors output_${j}.map.ccp4 box=True output_file_name=output_${j}.mtz
