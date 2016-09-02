5c7586d524f0e79c2ab4abe314e0c6f51f412f40
Kartik Ayyer <kartik.ayyer@desy.de>
2016-09-02 13:32:11 +0200

    zero_outer: Set outermost annulus of voxels to -1 to allow half-speckles to relax and not have a sharp edge. This also reduces the increase int he estimated background there"

--- utils/src/zero_outer.c
+++ utils/src/zero_outer.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <float.h>
+#include <math.h>
 
 char* extract_fname(char* fullName) {
 	return 
@@ -18,14 +20,15 @@ char* remove_ext(char *fullName) {
 }
 
 int main(int argc, char *argv[]) {
-	long x, y, z, size, c, rsq ;
-	float rmin, rminsq, rmax, rmaxsq ;
+	long x, y, z, size, c, rad ;
+	float rmin, rmax, minmodel = FLT_MAX ;
 	float *model ;
 	FILE *fp ;
 	char fname[500] ;
 	
 	if (argc < 5) {
 		fprintf(stderr, "Format: %s <model_fname> <size> <rmin> <rmax>\n", argv[0]) ;
+		fprintf(stderr, "Optional: <subtract_min>. Default 0.\n") ;
 		return 1 ;
 	}
 	size = atoi(argv[2]) ;
@@ -33,9 +36,6 @@ int main(int argc, char *argv[]) {
 	rmax = atof(argv[4]) ;
 	
 	c = size / 2 ;
-	rmaxsq = rmax*rmax ;
-	rminsq = rmin*rmin ;
-	
 	model = malloc(size*size*size * sizeof(float)) ;
 	
 	fp = fopen(argv[1], "rb") ;
@@ -46,16 +46,35 @@ int main(int argc, char *argv[]) {
 	for (x = 0 ; x < size ; ++x)
 	for (y = 0 ; y < size ; ++y)
 	for (z = 0 ; z < size ; ++z) {
-		rsq = (x-c)*(x-c) + (y-c)*(y-c) + (z-c)*(z-c) ;
+		rad = sqrtf((x-c)*(x-c) + (y-c)*(y-c) + (z-c)*(z-c)) ;
 		
-		if (rsq < rminsq)
+		if (rad < rmin)
 			model[x*size*size + y*size + z] = -1.f ;
-		if (rsq > rmaxsq)
+		else if (rad > rmax + 4)
 			model[x*size*size + y*size + z] = 0.f ;
+		else if (rad > rmax - 4)
+			model[x*size*size + y*size + z] = -1.f ;
+		else if (model[x*size*size + y*size + z] < minmodel)
+			minmodel = model[x*size*size + y*size + z] ;
 	}
 	fprintf(stderr, "Zeroed outer voxels and set inner voxels to be negative\n") ;
+	fprintf(stderr, "Minimum value in annulus = %.3e\n", minmodel) ;
 	
-	sprintf(fname, "%s-zero.raw", remove_ext(argv[1])) ;
+	if (argc > 5 && atoi(argv[5]) > 0) {
+		for (x = 0 ; x < size ; ++x)
+		for (y = 0 ; y < size ; ++y)
+		for (z = 0 ; z < size ; ++z) {
+			rad = sqrtf((x-c)*(x-c) + (y-c)*(y-c) + (z-c)*(z-c)) ;
+			
+			if (rad >= rmin && rad <= rmax - 4)
+				model[x*size*size + y*size + z] -= minmodel ;
+		}
+		fprintf(stderr, "Subtracted minimum value in annulus\n") ;
+		sprintf(fname, "%s-pos-zero.raw", remove_ext(argv[1])) ;
+	}
+	else {
+		sprintf(fname, "%s-zero.raw", remove_ext(argv[1])) ;
+	}
 	fprintf(stderr, "%s\n", fname) ;
 	fp = fopen(fname, "wb") ;
 	fwrite(model, sizeof(float), size*size*size, fp) ;
@@ -65,4 +84,3 @@ int main(int argc, char *argv[]) {
 	
 	return 0 ;
 }
-		
