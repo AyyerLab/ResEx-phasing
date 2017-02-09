fb399d0a8a24268661e52df8ec3f03a54a36c45a
Kartik Ayyer <kartik.ayyer@desy.de>
2017-02-09 17:20:22 +0100

    rad_avg: Fixed bug relating to data type of 3D model

--- utils/src/rad_avg.c
+++ utils/src/rad_avg.c
@@ -12,9 +12,10 @@ char* remove_ext(char *fullName) {
 }
 
 int main(int argc, char *argv[]) {
-	long x, y, z, size, c, vol ;
-	int num_bins, *count, *bin ;
-	double binsize, *model, *rad_avg ;
+	long x, y, z, vox, size, c, vol ;
+	int num_bins, *count, bin ;
+	double binsize, *rad_avg ;
+	float *model ;
 	FILE *fp ;
 	char fname[999] ;
 	
@@ -26,12 +27,11 @@ int main(int argc, char *argv[]) {
 	
 	vol = size*size*size ;
 	c = size / 2 ;
-	num_bins = size ;
+	num_bins = ceil(sqrt(3.) * size / 2.) + 1 ;
 	binsize = 1. ;
 	
 	model = malloc(vol * sizeof(float)) ;
-	bin = malloc(vol * sizeof(int)) ;
-	rad_avg = calloc(num_bins, sizeof(float)) ;
+	rad_avg = calloc(num_bins, sizeof(double)) ;
 	count = calloc(num_bins, sizeof(int)) ;
 	
 	fp = fopen(argv[1], "rb") ;
@@ -40,26 +40,27 @@ int main(int argc, char *argv[]) {
 	
 	for (x = 0 ; x < size ; ++x)
 	for (y = 0 ; y < size ; ++y)
-	for (z = 0 ; z < size ; ++z)
-		bin[x*size*size + y*size + z]
-		 = (int) (sqrt((x-c)*(x-c) + (y-c)*(y-c) + (z-c)*(z-c)) / binsize) ;
-	
-	for (x = 0 ; x < vol ; ++x) {
-		if (bin[x] >= num_bins)
+	for (z = 0 ; z < size ; ++z) {
+		vox = x*size*size + y*size + z ;
+		bin = (int) (sqrt((x-c)*(x-c) + (y-c)*(y-c) + (z-c)*(z-c)) / binsize) ;
+		if (bin <  0 || bin >= num_bins) {
+			fprintf(stderr, "bin[%ld, %ld, %ld] = %d > %d\n", x, y, z, bin, num_bins) ;
 			continue ;
+		}
 		
-		rad_avg[bin[x]] += model[x] ;
-		count[bin[x]]++ ;
+		rad_avg[bin] += model[vox] ;
+		count[bin]++ ;
 	}
 	
 	for (x = 0 ; x < num_bins ; ++x)
 	if (count[x] > 0)
 		rad_avg[x] /= count[x] ;
 	
-	for (x = 0 ; x < vol ; ++x)
-		model[x] = rad_avg[bin[x]] ;
+//	for (x = 0 ; x < vol ; ++x)
+//		model[x] = rad_avg[bin[x]] ;
 	
 	sprintf(fname, "%s-avg.bin", remove_ext(argv[1])) ;
+	fprintf(stderr, "Writing radial average to %s\n", fname) ;
 	fp = fopen(fname, "wb") ;
 	fwrite(rad_avg, sizeof(double), num_bins, fp) ;
 	fclose(fp) ;
@@ -70,11 +71,10 @@ int main(int argc, char *argv[]) {
 	if (count[x] > 0)
 		fprintf(fp, "%ld\t%.6e\n", x, rad_avg[x]) ;
 	fclose(fp) ;
-*/	
+*/
 	free(model) ;
 	free(rad_avg) ;
 	free(count) ;
-	free(bin) ;
 	
 	return 0 ;
 }
