c699a435735eb7c58f364f6bed1c885b5fe58dda
Kartik Ayyer <kartik.ayyer@desy.de>
2017-07-20 13:03:47 +0200

    scale_q: Calculating scale factor along all three axes separately

--- utils/src/scale_q.c
+++ utils/src/scale_q.c
@@ -43,20 +43,34 @@ int main(int argc, char *argv[]) {
 		obs_mag[x] = sqrt(obs_mag[x]) ;
 	
 	// Calculate scale factor
+	double cx, cy, cz ;
 	num_bins = (int) (size / binsize) ;
-	dot = calloc(num_bins, sizeof(double)) ;
-	normsq = calloc(num_bins, sizeof(double)) ;
-	num_vox = calloc(num_bins, sizeof(long)) ;
+	dot = calloc(3*num_bins, sizeof(double)) ;
+	normsq = calloc(3*num_bins, sizeof(double)) ;
+	num_vox = calloc(3*num_bins, sizeof(long)) ;
 	for (x = 0 ; x < size ; ++x)
 	for (y = 0 ; y < size ; ++y)
 	for (z = 0 ; z < size ; ++z) {
 		rad = sqrt((x-c)*(x-c) + (y-c)*(y-c) + (z-c)*(z-c)) ;
 		bin = (int) (rad / binsize) ;
+		cx = fabs((x-c)) / rad ;
+		cy = fabs((y-c)) / rad ;
+		cz = fabs((z-c)) / rad ;
 		vox = x*size*size + y*size + z ;
-		if (obs_mag[vox] > 0.) {
-			dot[bin] += obs_mag[vox] * model_mag[vox] ;
-			normsq[bin] += pow(obs_mag[vox], 2.) ;
-			num_vox[bin]++ ;
+		if (obs_mag[vox] > 0. && cx > 0.95) {
+			dot[0*num_bins + bin] += obs_mag[vox] * model_mag[vox] ;
+			normsq[0*num_bins + bin] += pow(obs_mag[vox], 2.) ;
+			num_vox[0*num_bins + bin]++ ;
+		}
+		else if (obs_mag[vox] > 0. && cy > 0.95) {
+			dot[1*num_bins + bin] += obs_mag[vox] * model_mag[vox] ;
+			normsq[1*num_bins + bin] += pow(obs_mag[vox], 2.) ;
+			num_vox[1*num_bins + bin]++ ;
+		}
+		else if (obs_mag[vox] > 0. && cz > 0.95) {
+			dot[2*num_bins + bin] += obs_mag[vox] * model_mag[vox] ;
+			normsq[2*num_bins + bin] += pow(obs_mag[vox], 2.) ;
+			num_vox[2*num_bins + bin]++ ;
 		}
 		if (y == size-1 && z == size-1)
 			fprintf(stderr, "\rFinished x = %ld", x+1) ;
@@ -66,9 +80,16 @@ int main(int argc, char *argv[]) {
 	// Write output to file
 	fp = fopen("data/scale_q.dat", "w") ;
 	for (bin = 0 ; bin < num_bins ; ++bin) {
-		if (normsq[bin] > 0.)
-			dot[bin] /= normsq[bin] ;
-		fprintf(fp, "%8.3f %.6e %ld\n", bin*binsize, dot[bin], num_vox[bin]) ;
+		if (normsq[0*num_bins + bin] > 0.)
+			dot[0*num_bins + bin] /= normsq[0*num_bins + bin] ;
+		if (normsq[1*num_bins + bin] > 1.)
+			dot[1*num_bins + bin] /= normsq[1*num_bins + bin] ;
+		if (normsq[2*num_bins + bin] > 2.)
+			dot[2*num_bins + bin] /= normsq[2*num_bins + bin] ;
+		fprintf(fp, "%8.3f %.6e %8ld %.6e %8ld %.6e %8ld\n", bin*binsize, 
+				dot[0*num_bins + bin], num_vox[0*num_bins + bin],
+				dot[1*num_bins + bin], num_vox[1*num_bins + bin],
+				dot[2*num_bins + bin], num_vox[2*num_bins + bin]) ;
 	}
 	
 	// Free memory
