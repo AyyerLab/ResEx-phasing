0885902fff69997f529ef25a3f0cf1fa05335a4f
Kartik Ayyer <kartik.ayyer@desy.de>
2018-11-17 11:24:29 +0100

    mapgen: Improved script to generate input maps from ccp4 output

--- scripts/mapgen.sh
+++ scripts/mapgen.sh
@@ -1,14 +1,36 @@
 #!/bin/zsh
 
-if [ $# -lt 2 ]
+# This script takes the output from a PHENIX Phaser or Refinement run
+# and produces a CCP4 map of a single asymmetric unit using cut_out_density
+# Parameters:
+# 	folder: Path to PHENIX output directory
+# 	folder_type: The string 'phaser' or 'refine'
+# 	res_cutoff: High resolution cutoff in Angstroms
+# 	tag: Prefix of output file
+# Output:
+# 	<tag>_2mFo-DFc.ccp4 file in the folder from which the script is launched
+
+if [ $# -lt 4 ]
 then
-	echo Format: $0 folder tag
+	echo Format: $0 folder folder_type res_cutoff tag
 	exit
 fi
 
 cd $1
-phenix.cut_out_density high_resolution=2.5 cutout_type=model cutout_model_radius=2.0 *refine*.pdb `ls *refine*.mtz|grep -v data`
-#phenix.cut_out_density high_resolution=3.0 cutout_type=model cutout_model_radius=2.0 *phaser*.pdb *phaser*.mtz
-phenix.fft prefix=$2 scale=volume cutout.mtz
-cp ${2}*.ccp4 $OLDPWD
+folder_type=$2
+res_cutoff=$3
+if [ $folder_type = 'phaser' ]
+	phenix.cut_out_density high_resolution=3.0 cutout_type=model \
+		cutout_model_radius=${res_cutoff} *phaser*.pdb *phaser*.mtz
+elif [ $folder_type = 'refine' ]
+	phenix.cut_out_density high_resolution=2.5 cutout_type=model \
+		cutout_model_radius=${res_cutoff} *refine*.pdb `ls *refine*.mtz|grep -v data`
+else
+	echo Unknown folder_type, $2
+	exit
+fi
+
+phenix.fft prefix=$4 scale=volume cutout.mtz
+cp ${4}*.ccp4 $OLDPWD
 cd -
+ls -l ${4}*.ccp4
