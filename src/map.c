f66bf3bac75ea5ef42d8f7e40fcaf2bd79285334
Kartik Ayyer <kartik.ayyer@desy.de>
2019-01-15 23:31:05 +0100

    map: Added parse, write and free methods

--- src/map.c
+++ src/map.c
@@ -1,24 +1,100 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "map.h"
 
-struct ccp4_map {
-	int nx, ny, nz ;
-	int mode ;
-	int nxstart, nystart, nzstart ;
-	int mx, my, mz ;
-	float xlen, ylen, zlen ;
-	float alpha, beta, gamma ;
-	int mapc, mapr, maps ;
-	float dmin, dmax, dmean ;
-	int ispg ;
-	int nsymbt ;
-	char extra[100] ;
-	float xorg, yorg, zorg ;
-	char cmap[4] ;
-	char machst[4] ;
-	float rms ;
-	int nlabl ;
-	char labels[10][80] ;
-} ;
+int parse_map(char *fname, struct ccp4_map *map) {
+	int nx, ny,nz, mode ;
+	long vol ;
+	FILE *fp ;
+	
+	fp = fopen(fname, "rb") ;
+	if (fp == NULL) {
+		fprintf(stderr, "Cannot open file %s\n", fname) ;
+		return 1 ;
+	}
+	fread(&(map->header), sizeof(struct ccp4_header), 1, fp) ;
+	fseek(fp, map->header.nsymbt, SEEK_CUR) ;
+	
+	mode = map->header.mode ;
+	nx = map->header.nx ;
+	ny = map->header.ny ;
+	nz = map->header.nz ;
+	vol = nx*ny*nz ;
+	
+	if (mode == 0) {
+		fprintf(stderr, "Parsing (%d, %d, %d) int8_t volume\n", nx, ny, nz) ;
+		map->i8_data = malloc(vol * sizeof(int8_t)) ;
+		fread(map->i8_data, sizeof(int8_t), vol, fp) ;
+	}
+	else if (mode == 1) {
+		fprintf(stderr, "Parsing (%d, %d, %d) int16_t volume\n", nx, ny, nz) ;
+		map->i16_data = malloc(vol * sizeof(int16_t)) ;
+		fread(map->i16_data, sizeof(int16_t), vol, fp) ;
+	}
+	else if (mode == 2) {
+		fprintf(stderr, "Parsing (%d, %d, %d) float volume\n", nx, ny, nz) ;
+		map->data = malloc(vol * sizeof(float)) ;
+		fread(map->data, sizeof(float), vol, fp) ;
+	}
+	else if (mode == 4) {
+		fprintf(stderr, "Parsing (%d, %d, %d) float complex volume\n", nx, ny, nz) ;
+		map->c8_data = malloc(vol * sizeof(float complex)) ;
+		fread(map->c8_data, sizeof(float complex), vol, fp) ;
+	}
+	else if (mode == 6) {
+		fprintf(stderr, "Parsing (%d, %d, %d) uint16_t volume\n", nx, ny, nz) ;
+		map->u16_data = malloc(vol * sizeof(uint16_t)) ;
+		fread(map->u16_data, sizeof(uint16_t), vol, fp) ;
+	}
+	fclose(fp) ;
+	
+	return 0 ;
+}
 
+int write_map(char *fname, struct ccp4_map *map) {
+	int nx, ny, nz, mode ;
+	long vol ;
+	FILE *fp ;
+	
+	fp = fopen(fname, "wb") ;
+	if (fp == NULL) {
+		fprintf(stderr, "Cannot open file %s\n", fname) ;
+		return 1 ;
+	}
+	mode = map->header.mode ;
+	nx = map->header.nx ;
+	ny = map->header.ny ;
+	nz = map->header.nz ;
+	vol = nx*ny*nz ;
+	
+	fwrite(&(map->header), sizeof(struct ccp4_header), 1, fp) ;
+	if (map->header.nsymbt > 0)
+		fwrite(&(map->ext_header), sizeof(char), map->header.nsymbt, fp) ;
+	
+	if (mode == 0)
+		fwrite(map->i8_data, sizeof(int8_t), vol, fp) ;
+	else if (mode == 1)
+		fwrite(map->i16_data, sizeof(int16_t), vol, fp) ;
+	else if (mode == 2)
+		fwrite(map->data, sizeof(float), vol, fp) ;
+	else if (mode == 4)
+		fwrite(map->c8_data, sizeof(float complex), vol, fp) ;
+	else if (mode == 6)
+		fwrite(map->u16_data, sizeof(uint16_t), vol, fp) ;
+	fclose(fp) ;
+	
+	return 0 ;
+}
 
+void free_map(struct ccp4_map *map) {
+	if (map->ext_header != NULL)
+		free(map->ext_header) ;
+	if (map->i8_data != NULL)
+		free(map->i8_data) ;
+	if (map->i16_data != NULL)
+		free(map->i16_data) ;
+	if (map->data != NULL)
+		free(map->data) ;
+	if (map->c8_data != NULL)
+		free(map->c8_data) ;
+	if (map->u16_data != NULL)
+		free(map->u16_data) ;
+}
---
+++ src/map.h
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <complex.h>
+
+struct ccp4_header {
+	int32_t nx, ny, nz ;
+	int32_t mode ;
+	int32_t nxstart, nystart, nzstart ;
+	int32_t mx, my, mz ;
+	float xlen, ylen, zlen ;
+	float alpha, beta, gamma ;
+	int32_t mapc, mapr, maps ;
+	float dmin, dmax, dmean ;
+	int32_t ispg ;
+	int32_t nsymbt ;
+	char extra[100] ;
+	float xorig, yorig, zorig ;
+	char cmap[4] ;
+	char machst[4] ;
+	float rms ;
+	int32_t nlabl ;
+	char labels[10][80] ;
+} ;
+
+struct ccp4_map {
+	struct ccp4_header header ;
+	char *ext_header ;
+	
+	int8_t *i8_data ;
+	int16_t *i16_data ;
+	float *data ;
+	float complex *c8_data ;
+	uint16_t *u16_data ;
+} ;
+
+int parse_map(char*, struct ccp4_map*) ;
+int write_map(char*, struct ccp4_map*) ;
+void free_map(struct ccp4_map*) ;
