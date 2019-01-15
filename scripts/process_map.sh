7d31eb38cca380f144e6a5097ee0e3c5453de1ba
Kartik Ayyer <kartik.ayyer@desy.de>
2019-01-15 15:47:53 +0100

    process_map: Now works when run from anywhere

--- scripts/process_map.sh
+++ scripts/process_map.sh
@@ -53,10 +53,13 @@ then
 	echo Only doing support wrapping
 fi
 
+# Script utils directory
+utils=$(cd $(dirname "${BASH_SOURCE[0]}")/../utils; pwd)
+
 # Calculate filenames
 map_name=`basename $1`
 mapnoext="${map_name%.*}"
-log_name=results/${mapnoext}.log
+log_name=data/logs/${mapnoext}.log
 echo Log file: $log_name
 strmodel=data/convert/${mapnoext}-str.cpx
 lowresmodel_name=data/convert/${mapnoext}-recon.raw
@@ -69,25 +72,25 @@ if [ $skip -eq 0 ]
 then
 	echo $1 > $log_name
 	echo ./utils/read_map $1 $res | tee $log_name
-	./utils/read_map $1 $res >> $log_name 2>&1
+	${utils}/read_map $1 $res >> $log_name 2>&1
 	
 	padmodel=`grep "Saving padded" $log_name|awk '{print $5}'`
 	padsize=`grep "volume size" $log_name|awk '{print $5}'`
 	padnoext="${padmodel%.*}"
 	echo -------------------------------------------------------------------------------- >> $log_name
 	echo ./utils/gen_fdens $padmodel ${padnoext}-fdens.cpx 1| tee -a $log_name
-	./utils/gen_fdens $padmodel ${padnoext}-fdens.cpx 1 >> $log_name 2>&1
+	${utils}/gen_fdens $padmodel ${padnoext}-fdens.cpx 1 >> $log_name 2>&1
 	
 	sx=`grep Stretch $log_name|awk -F'[=,()]' '{print $3}'`
 	sy=`grep Stretch $log_name|awk -F'[=,()]' '{print $4}'`
 	sz=`grep Stretch $log_name|awk -F'[=,()]' '{print $5}'`
 	echo -------------------------------------------------------------------------------- >> $log_name
 	echo ./utils/fstretch ${padnoext}-fdens.cpx $size $sx $sy $sz $strmodel | tee -a $log_name
-	./utils/fstretch ${padnoext}-fdens.cpx $size $sx $sy $sz $strmodel >> $log_name 2>&1
+	${utils}/fstretch ${padnoext}-fdens.cpx $size $sx $sy $sz $strmodel >> $log_name 2>&1
 	
 	echo -------------------------------------------------------------------------------- >> $log_name
 	echo ./utils/gen_dens $strmodel $lowresmodel_name | tee -a $log_name
-	./utils/gen_dens $strmodel $lowresmodel_name >> $log_name 2>&1
+	${utils}/gen_dens $strmodel $lowresmodel_name >> $log_name 2>&1
 	
 	echo -------------------------------------------------------------------------------- >> $log_name
 	echo Removing temporary files
@@ -99,7 +102,7 @@ if [ $skip -lt 2 ]
 then
 	echo -------------------------------------------------------------------------------- >> $log_name
 	echo ./utils/create_support $lowresmodel_name $supp_radius $supp_thresh $supp_name | tee -a $log_name
-	./utils/create_support $lowresmodel_name $supp_radius $supp_thresh $supp_name >> $log_name 2>&1
+	${utils}/create_support $lowresmodel_name $supp_radius $supp_thresh $supp_name >> $log_name 2>&1
 	
 	echo -------------------------------------------------------------------------------- >> $log_name
 	echo Constraining lowresmodel by support | tee -a $log_name
@@ -113,10 +116,10 @@ then
 	
 	echo -------------------------------------------------------------------------------- >> $log_name
 	echo ./utils/gen_fdens $supprecon_name $cpx_name $point_group | tee -a $log_name
-	./utils/gen_fdens $supprecon_name $cpx_name $point_group >> $log_name 2>&1
+	${utils}/gen_fdens $supprecon_name $cpx_name $point_group >> $log_name 2>&1
 	
 	echo -------------------------------------------------------------------------------- >> $log_name
 	echo ./utils/create_support $supprecon_name $supp_radius $supp_thresh $supp_name | tee -a $log_name
-	./utils/create_support $supprecon_name $supp_radius $supp_thresh $supp_name >> $log_name 2>&1
+	${utils}/create_support $supprecon_name $supp_radius $supp_thresh $supp_name >> $log_name 2>&1
 fi
 
