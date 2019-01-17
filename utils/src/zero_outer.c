77300c83da84b31f36ce0a82b5773af0d7edcab1
Kartik Ayyer <kartik.ayyer@desy.de>
2019-01-17 17:42:29 +0100

    zero_outer: Using CCP4 map I/O

--- utils/src/zero_outer.c
+++ utils/src/zero_outer.c
@@ -4,13 +4,13 @@
 #include <float.h>
 #include <math.h>
 #include "../../src/utils.h"
+#include "../../src/map.h"
 
 int main(int argc, char *argv[]) {
-	long x, y, z, size, c, vol ;
+	long x, y, z, size, c ;
 	float rad, rmin, rmax, minmodel = FLT_MAX ;
-	float *model ;
-	FILE *fp ;
-	char fname[500] ;
+	char fname[1024] ;
+	struct ccp4_map map = {0} ;
 	
 	if (argc < 4) {
 		fprintf(stderr, "Zero Outer: Process outer and inner parts of intensity file\n") ;
@@ -18,21 +18,23 @@ int main(int argc, char *argv[]) {
 		fprintf(stderr, "voxels outside <rmax> are zeroed and ones inside <rmin> are set to -1.\n") ;
 		fprintf(stderr, "If <subtract_min> is non-zero, the minimum value in each radial shell is subtracted\n") ;
 		fprintf(stderr, "\nUsage: %s <intens_fname> <rmin> <rmax>\n", argv[0]) ;
-		fprintf(stderr, "Optional: <subtract_min>. Default 0.\n") ;
-		fprintf(stderr, "\nOutput: <intens_fname>-zero.raw or <intens_fname>-pos-zero.raw\n") ;
+		fprintf(stderr, "\nOutput: <intens_fname>-zero.ccp4\n") ;
 		return 1 ;
 	}
-	size = get_size(argv[1], sizeof(float)) ;
 	rmin = atof(argv[2]) ;
 	rmax = atof(argv[3]) ;
 	
-	vol = size*size*size ;
+	parse_map(argv[1], &map) ;
+	if (map.f32_data == NULL) {
+		fprintf(stderr, "Need float data in input intens\n") ;
+		return 1 ;
+	}
+	if (map.header.nx != map.header.ny || map.header.nx != map.header.nz) {
+		fprintf(stderr, "Need cubic volume of data (input shape: %d, %d, %d)\n", map.header.nx, map.header.ny, map.header.nz) ;
+		return 1 ;
+	}
+	size = map.header.nx ;
 	c = size / 2 ;
-	
-	model = malloc(vol * sizeof(float)) ;
-	fp = fopen(argv[1], "rb") ;
-	fread(model, sizeof(float), vol, fp) ;
-	fclose(fp) ;
 	fprintf(stderr, "Read model\n") ;
 	
 	for (x = 0 ; x < size ; ++x)
@@ -41,38 +43,24 @@ int main(int argc, char *argv[]) {
 		rad = sqrtf((x-c)*(x-c) + (y-c)*(y-c) + (z-c)*(z-c)) ;
 		
 		if (rad < rmin)
-			model[x*size*size + y*size + z] = -1.f ;
+			map.f32_data[x*size*size + y*size + z] = -1.f ;
 		else if (rad > rmax + 3)
-			model[x*size*size + y*size + z] = 0.f ;
+			map.f32_data[x*size*size + y*size + z] = 0.f ;
 		else if (rad > rmax - 3)
-			model[x*size*size + y*size + z] = -1.f ;
-		else if (model[x*size*size + y*size + z] < minmodel && model[x*size*size + y*size + z] > -1000.)
-			minmodel = model[x*size*size + y*size + z] ;
+			map.f32_data[x*size*size + y*size + z] = -1.f ;
+		else if (map.f32_data[x*size*size + y*size + z] < minmodel && map.f32_data[x*size*size + y*size + z] > -1000.)
+			minmodel = map.f32_data[x*size*size + y*size + z] ;
 	}
 	fprintf(stderr, "Zeroed outer voxels and set inner voxels to be negative\n") ;
 	fprintf(stderr, "Minimum value in annulus = %.3e\n", minmodel) ;
 	
-	if (argc > 4 && atoi(argv[4]) > 0) {
-		for (x = 0 ; x < size ; ++x)
-		for (y = 0 ; y < size ; ++y)
-		for (z = 0 ; z < size ; ++z) {
-			rad = sqrtf((x-c)*(x-c) + (y-c)*(y-c) + (z-c)*(z-c)) ;
-			
-			if (rad >= rmin && rad <= rmax - 3)
-				model[x*size*size + y*size + z] -= minmodel ;
-		}
-		fprintf(stderr, "Subtracted minimum value in annulus\n") ;
-		sprintf(fname, "%s-pos-zero.raw", remove_ext(argv[1])) ;
-	}
-	else {
-		sprintf(fname, "%s-zero.raw", remove_ext(argv[1])) ;
-	}
-	fprintf(stderr, "%s\n", fname) ;
-	fp = fopen(fname, "wb") ;
-	fwrite(model, sizeof(float), vol, fp) ;
-	fclose(fp) ;
+	sprintf(map.header.labels, "ResEx-phasing:zero_outer %s\n", extract_fname(argv[1])) ;
+	map.header.rms = -1. ;
+	sprintf(fname, "%s-zero.ccp4", remove_ext(argv[1])) ;
+	fprintf(stderr, "Saving to %s\n", fname) ;
+	write_map(fname, &map) ;
 	
-	free(model) ;
+	free_map(&map) ;
 	
 	return 0 ;
 }
