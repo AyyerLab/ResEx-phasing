b992c3469344c16986ed950e3bfae2939da955d6
Kartik Ayyer <kartik.ayyer@desy.de>
2019-01-18 13:02:02 +0100

    calc_scale: Using maps as input

--- utils/src/calc_scale.c
+++ utils/src/calc_scale.c
@@ -4,12 +4,14 @@
 #include <math.h>
 #include <locale.h>
 #include "../../src/utils.h"
+#include "../../src/map.h"
 
 int main(int argc, char *argv[]) {
-	long x, y, z, size, c, vol, num_vox = 0 ;
+	long x, y, z, vox ;
+	long size, c, vol, num_vox = 0 ;
 	double dot = 0., normsq = 0., rsq, scale, rmin, rmax ;
-	float *obs_mag, *model_mag ;
-	FILE *fp ;
+	struct ccp4_map obs_map = {0}, model_map = {0} ;
+	setlocale(LC_ALL, "C") ; // For commas in large integers
 	
 	if (argc < 5) {
 		fprintf(stderr, "Calc Scale: Calculates scale factor between two intensities\n") ;
@@ -18,46 +20,46 @@ int main(int argc, char *argv[]) {
 		fprintf(stderr, "\nUsage: %s <sym_model_fname> <merge_fname> <rmin> <rmax>\n", argv[0]) ;
 		return 1 ;
 	}
-	size = get_size(argv[1], sizeof(float)) ;
 	rmin = atof(argv[3]) ;
 	rmax = atof(argv[4]) ;
 	
-	setlocale(LC_ALL, "C") ; // For commas in large integers
+	// Parse sym_model and obs_map
+	if (parse_map(argv[1], &model_map))
+		return 1 ;
+	if (model_map.f32_data == NULL) {
+		fprintf(stderr, "sym_model needs float data\n") ;
+		return 1 ;
+	}
+	if (parse_map(argv[2], &model_map))
+		return 1 ;
+	if (obs_map.f32_data == NULL) {
+		fprintf(stderr, "merge needs float data\n") ;
+		return 1 ;
+	}
+	
+	size = model_map.header.nx ;
 	c = size / 2 ;
 	vol = size*size*size ;
 	
-	// Parse complex model
-	model_mag = malloc(vol * sizeof(float)) ;
-	fp = fopen(argv[1], "rb") ;
-	fread(model_mag, sizeof(float), vol, fp) ;
-	fclose(fp) ;
-	
 	// Calculate magnitude
-	for (x = 0 ; x < vol ; ++x)
-		model_mag[x] = sqrt(model_mag[x]) ;
-	
-	// Parse experimental merge
-	obs_mag = malloc(vol * sizeof(float)) ;
-	fp = fopen(argv[2], "rb") ;
-	fread(obs_mag, sizeof(float), vol, fp) ;
-	fclose(fp) ;
-	
-	// Calculate magnitude
-	for (x = 0 ; x < vol ; ++x)
-	if (obs_mag[x] > 0.)
-		obs_mag[x] = sqrt(obs_mag[x]) ;
+	for (x = 0 ; x < vol ; ++x) {
+		model_map.f32_data[x] = sqrtf(model_map.f32_data[x]) ;
+		if (obs_map.f32_data[x] > 0.f)
+			obs_map.f32_data[x] = sqrtf(obs_map.f32_data[x]) ;
+	}
 	
 	// Calculate scale factor
 	for (x = 0 ; x < size ; ++x)
 	for (y = 0 ; y < size ; ++y)
 	for (z = 0 ; z < size ; ++z) {
 		rsq = (x-c)*(x-c) + (y-c)*(y-c) + (z-c)*(z-c) ;
+		vox = x*size*size + y*size + z ;
 		
-		if (rsq > rmin*rmin && rsq < rmax*rmax && obs_mag[x*size*size + y*size + z] > 0.) {
-			//dot += model_mag[x*size*size + y*size + z] ;
-			//normsq += obs_mag[x*size*size + y*size + z] ;
-			dot += obs_mag[x*size*size + y*size + z] * model_mag[x*size*size + y*size + z] ;
-			normsq += pow(obs_mag[x*size*size + y*size + z], 2.) ;
+		if (rsq > rmin*rmin && rsq < rmax*rmax && obs_map.f32_data[vox] > 0.) {
+			//dot += model_map.f32_data[vox] ;
+			//normsq += obs_map.f32_data[vox] ;
+			dot += obs_map.f32_data[vox] * model_map.f32_data[vox] ;
+			normsq += pow(obs_map.f32_data[vox], 2.) ;
 			num_vox++ ;
 		}
 	}
@@ -68,8 +70,8 @@ int main(int argc, char *argv[]) {
 	printf("%'ld voxels contributed to this calculation\n", num_vox) ;
 	
 	// Free memory
-	free(model_mag) ;
-	free(obs_mag) ;
+	free_map(&model_map) ;
+	free_map(&obs_map) ;
 	
 	return 0 ;
 }
