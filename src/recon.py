70afa0a6fc5f83e280933ec78aa50cb33fc30ece
Kartik Ayyer <kartik.ayyer@desy.de>
2019-03-14 14:25:01 +0100

    recon: Included ability to do and average over outer loops

--- src/recon.py
+++ src/recon.py
@@ -24,13 +24,16 @@ def main():
     average_p1 = numpy.zeros(phas.p1.shape, dtype='f4')
     average_p2 = numpy.zeros(phas.p1.shape, dtype='f4')
 
-    for i in range(1, phas.num_iter + phas.num_avg_iter + 1):
+    for i in range(phas.num_loops * (phas.num_iter + phas.num_avg_iter)):
         time1 = time.time()
 
-        error = phas.run_iteration(i)
+        if i > 0 and i % (phas.num_iter+phas.num_avg_iter) == 0:
+            phas.allocate_memory()
+            phas.io.init_iterate(phas.proj, phas.iterate)
+        error = phas.run_iteration(i%(phas.num_iter+phas.num_avg_iter) + 1)
         if error < 0:
             sys.exit(1)
-        if i > phas.num_iter:
+        if (phas.num_loops == 1 and i > phas.num_iter) or (phas.num_loops > 1 and i%phas.num_iter == phas.num_iter - 1):
             if CUDA:
                 average_p1 += phas.p1.get()
                 average_p2 += phas.p2.get()
@@ -39,9 +42,12 @@ def main():
                 average_p2 += phas.p2
 
         time2 = time.time()
-        phas.io.save_current(phas, phas.proj, i, time1, time2, error)
-        sys.stderr.write("\rFinished %d/%d iterations. " % (i, phas.num_iter+phas.num_avg_iter))
-        if i > phas.num_iter:
+        phas.io.save_current(phas, phas.proj, i+1, time1, time2, error)
+        sys.stderr.write('\r')
+        if phas.num_loops > 1:
+            sys.stderr.write("Loop %d/%d: " % (i // phas.num_iter + 1, phas.num_loops))
+        sys.stderr.write("Finished %d/%d iterations. (%f) " % (i%(phas.num_iter+phas.num_avg_iter) + 1, phas.num_iter+phas.num_avg_iter, error))
+        if phas.num_loops == 1 and i > phas.num_iter:
             sys.stderr.write("Now averaging")
     sys.stderr.write("\nCalculating prtf and writing to file.\n")
 
@@ -50,11 +56,11 @@ def main():
         average_p2 /= phas.num_avg_iter
     else:
         if CUDA:
-            average_p1 = phas.p1.get()
-            average_p2 = phas.p2.get()
+            average_p1 = phas.p1.get() / phas.num_loops
+            average_p2 = phas.p2.get() / phas.num_loops
         else:
-            average_p1 = phas.p1
-            average_p2 = phas.p2
+            average_p1 = phas.p1 / phas.num_loops
+            average_p2 = phas.p2 / phas.num_loops
 
     #phas.calc_prtf(100)
     phas.io.save_output(phas, phas.proj, average_p1, average_p2)
